8349168e79049308784a3b9867dba7ae07637e51
lujiefan <jfjiefan@gmail.com>
2023-11-24 17:01:10 +0800

    add dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,18 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import Antd from "ant-design-vue";
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue";
+import router from "./router";
 
-const app = createApp(App)
+import "ant-design-vue/dist/reset.css";
 
-app.use(createPinia())
-app.use(router)
+const app = createApp(App);
 
-app.mount('#app')
+app.use(createPinia());
+app.use(router);
+app.use(Antd);
+
+app.mount("#app");
